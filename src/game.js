8eb6164ce7be2c37ff8b785fb8f7915a1e1706bf
Dale Karp <dale@dale.io>
2015-01-01 02:18:00 -0500

    Updating index.html with proper browserify bundle file; create phaser game & associated states in game.js

--- src/game.js
+++ src/game.js
@@ -1,3 +1,8 @@
 'use strict';
 
-var game = new Phaser.Game(1920, 1080, Phaser.AUTO);
+window.game = new Phaser.Game(1920, 1080, Phaser.AUTO);
+
+game.state.add('boot', require('./states/boot.js') );
+game.state.add('load', require('./states/load.js') );
+game.state.add('menu', require('./states/menu.js') );
+game.state.start('boot');
