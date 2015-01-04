69eed4c838b8737781f419eb6c228e574747d9a1
Dale Karp <dale@dale.io>
2015-01-04 02:01:49 -0500

    adding code for game state, now working at 720p

--- src/game.js
+++ src/game.js
@@ -1,6 +1,7 @@
-window.game = new Phaser.Game(1920, 1080, Phaser.AUTO);
+window.game = new Phaser.Game(1280, 720, Phaser.AUTO);
 
 game.state.add('boot', require('./states/boot.js') );
 game.state.add('load', require('./states/load.js') );
 game.state.add('menu', require('./states/menu.js') );
+game.state.add('game', require('./states/game.js') );
 game.state.start('boot');
--- src/states/menu.js
+++ src/states/menu.js
@@ -4,5 +4,6 @@ module.exports = {
     },
     create: function () {
         console.log('create: in menu state');
+        game.state.start('game');
     }
 };
