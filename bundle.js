078cc4a33566463c768d30fa2df965cb9a9e5447
Dale Karp <dale@dale.io>
2015-01-08 03:14:59 -0500

    updating...

--- bundle.js
+++ bundle.js
@@ -47,11 +47,11 @@ module.exports = {
     keyUIFull: null,
     // methods
     preload: function () {
-        game.load.tilemap('map', '../../assets/levels/level1.json', null, Phaser.Tilemap.TILED_JSON);
-        game.load.image('Tileset', '../../assets/images/spritesheet.png');
-        game.load.image('background', '../../assets/images/bg.png');
+        game.load.tilemap('map', '/assets/levels/level1.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('Tileset', '/assets/images/spritesheet.png');
+        game.load.image('background', '/assets/images/bg.png');
         // spritesheet(key to use, url, tile width, tile height, ???, sheet margin, tile padding)
-        game.load.spritesheet('Player', '../../assets/images/spritesheet.png', 21, 21, -1, 2, 2);
+        game.load.spritesheet('Player', '/assets/images/spritesheet.png', 21, 21, -1, 2, 2);
     },
     create: function () {
         console.log('create: in game state');
