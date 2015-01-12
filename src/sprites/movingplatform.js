a4d992b1bc08a52d6a4cb7aff07d1c800e116000
Dale Karp <dale@dale.io>
2015-01-11 23:40:36 -0500

    movingplatform now immovable, support for moving left and right

--- src/sprites/movingplatform.js
+++ src/sprites/movingplatform.js
@@ -9,14 +9,23 @@ var MovingPlatform = function (game, startingX, startingY, direction, speed, dis
     this.distance = distance;
     this.destination = { x: 0, y: 0 };
 
-
     // physics properties
     game.physics.enable(this);
     this.body.allowGravity = false;
+    this.body.immovable = true;
 
     // decide where the platform will be going
     if (this.direction === 'down') {
         this.destination.y = this.body.y + this.distance;
+    }
+    else if (this.direction === 'up') {
+        this.destination.y = this.body.y - this.distance;
+    }
+    else if (this.direction === 'left') {
+        this.destination.x = this.body.x - this.distance;
+    }
+    else if (this.direction === 'right') {
+        this.destination.x = this.body.x + this.distance;
     }
 
     // add sprite to game stage/world
@@ -50,7 +59,28 @@ MovingPlatform.prototype.update = function () {
             this.direction = 'down';
         }
     }
-    // TODO: implement left/right movement support
+    else if (this.direction === 'right') {
+        if (this.body.x + this.speed < this.destination.x) {
+            this.body.velocity.x = this.speed;
+        }
+        else {
+            difference = this.destination.x - this.body.x;
+            this.body.velocity.x = difference;
+            this.destination.x = this.body.x - this.distance;
+            this.direction = 'left';
+        }
+    }
+    else if (this.direction === 'left') {
+        if (this.body.x - this.speed > this.destination.x) {
+            this.body.velocity.x = -this.speed;
+        }
+        else {
+            difference = this.body.x - this.destination.x;
+            this.body.velocity.x = difference;
+            this.destination.x = this.body.x + this.distance;
+            this.direction = 'right';
+        }
+    }
 };
 
 module.exports = MovingPlatform;
